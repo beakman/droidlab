e86a079522266b4ada4edd63238a7bc4e96f8adf
Paco <psalido@gmail.com>
2016-10-11 12:24:15 +0200

    add api menu entry

--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     url(r'^accounts/', include('allauth.urls')),
 
     # Your stuff: custom urls includes go here
-    url(r'^api/', include('experiments.urls', namespace='experiments')),
+    url(r'^api/', include('experiments.urls', namespace='experiments', name='api')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
