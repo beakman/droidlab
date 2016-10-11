867d135062294ff818dc1ea047e46e379c462ced
beakman <beakman@users.noreply.github.com>
2016-10-11 14:21:53 +0200

    Add base_name to api

--- experiments/urls.py
+++ experiments/urls.py
@@ -3,6 +3,6 @@ from rest_framework import routers
 from experiments.views import ExperimentList
 
 router = routers.DefaultRouter()
-router.register(r'experiments', ExperimentList)
+router.register(r'experiments', ExperimentList, base_name='experiments')
 
 urlpatterns = router.urls
