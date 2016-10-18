0b08449deec96be1a9883846102ca46e4ccc787b
Francisco Salido <fsalido@hispasec.com>
2016-10-18 06:21:33 +0200

    fix experiments api endpoint

--- droidlab/api/urls.py
+++ droidlab/api/urls.py
@@ -3,16 +3,15 @@ from django.conf.urls import url, include
 from droidlab.experiments.views import ResultDetail, ResultList, ExperimentDetail, ExperimentList
 from droidlab.users.views import ListUsers, UserDetail, RetrieveCurrentUser
 
-# Experiments api endpoints
 urlpatterns = [
+    
+    # Experiments api endpoints
     url(r'^experiments/(?P<name>[-\w.]+)/results/(?P<pk>\d+)$', ResultDetail.as_view(), name='result-detail'),
     url(r'^experiments/(?P<name>[-\w.]+)/results$', ResultList.as_view(), name='result-list'),
     url(r'^experiments/(?P<name>[-\w.]+)$', ExperimentDetail.as_view(), name='experiment-detail'),
-    url(r'^experiments$', ExperimentList.as_view(), name='experiment-list')
-]
-
-# Users api endpoints
-urlpatterns = [
+    url(r'^experiments$', ExperimentList.as_view(), name='experiment-list'),
+    
+    # Users api endpoints
     url(r'^users$', ListUsers.as_view()),
     url(r'^users/(?P<pk>[0-9]+)/$', UserDetail.as_view()),
     url(r'^me', RetrieveCurrentUser.as_view()),
